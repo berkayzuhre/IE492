3cea6b2b08b12e8c2aea96e7dac18937398d76ed
Muhammed Karakurt <47485951+muhkarakurt1@users.noreply.github.com>
2020-05-14 15:09:58 +0300

    final

--- BU2020_TrainingHandlingTours.py
+++ BU2020_TrainingHandlingTours.py
@@ -61,7 +61,7 @@ RouteConditions1 = {
 	
 	# StartTime in Hour und Minute, MinDuration, MaxDuration in minutes (mandatory condition)
 	# determines earliest and latest arrival to end station
-	Cond.StartTimeAndDuration: (8, 0, 50, 100),		
+	Cond.StartTimeAndDuration: (8, 0, 60, 120),		
 	
 	# Max Wartezeit bei einer Haltestelle in Minuten (mandatory condition)
 	Cond.MaxWaitingTimeAtStation: (30,),			
@@ -83,7 +83,7 @@ RouteConditions1 = {
 	Cond.ReportDuringRouteSearch: (10,), 
 
 	# return routes found in x seconds
-	Cond.MaxSearchTimeInSeconds: (30,),
+	Cond.MaxSearchTimeInSeconds: (10,),
 	}
 
 RelevantLineCategories = ['S']		# ['RE','R','IR','IC','ICE','S']
@@ -159,25 +159,25 @@ def TEST_FindAndDisplayRoutes():
 		# save variable to file
 		#SaveVariableToFile(RouteInfoList1, PlanYear, PlanMonth, 'RouteInfoList1', directory=VariableDirectory)
 
-	N = 10
+	#N = 10
 
-	print "\nDisplay first %s routes in RouteInfoList1" % N
-	ctr = 0
-	for RouteInfo in RouteInfoList1:
-		ctr += 1 
-		if ctr > N: break
+	# print "\nDisplay first %s routes in RouteInfoList1" % N
+	# ctr = 0
+	# for RouteInfo in RouteInfoList1:
+	# 	ctr += 1 
+	# 	if ctr > N: break
 		
-		print "\nRoute-" + str(ctr)
+	# 	print "\nRoute-" + str(ctr)
 
-		# print raw RouteInfo
-		print "\nRaw RouteInfo:"
-		for conn in RouteInfo:
-			conn = list(conn)
-			conn[ConnInfoInd['trafficdays_hexcode']] = '-'
-			print tuple(conn)
+	# 	# print raw RouteInfo
+	# 	print "\nRaw RouteInfo:"
+	# 	for conn in RouteInfo:
+	# 		conn = list(conn)
+	# 		conn[ConnInfoInd['trafficdays_hexcode']] = '-'
+	# 		print tuple(conn)
 
-		print "\nRouteInfo:"
-		print PrettyStringRouteInfo(RouteInfo)
+	# 	print "\nRouteInfo:"
+	# 	print PrettyStringRouteInfo(RouteInfo)
 
 # test module
 if __name__ == '__main__':
@@ -205,29 +205,216 @@ if __name__ == '__main__':
 
 	# In order to calculate the Line Measurement (LM) coverage of a route,
 	# we need to have first LMRequirementsAll per LineKey (LineID, TimeWindow, WeekdayGroup):
+	
 	LMRequirementsAll = {
-		('7.S3a', 2, 11): 	2,
-		('7.S3a', 2, 12): 	2,
-		('7.S3a', 2, 13):	2,
-
-		('7.S6b', 3, 11): 	2,
-		('7.S6b', 3, 12): 	2,
-		('7.S6b', 3, 13):	2,
-
-		('7.S24b', 2, 11): 	2,
-		('7.S24b', 2, 12): 	2,
-		('7.S24b', 2, 13):	2,
-
-		('3.S52', 3, 11): 	2,
-		('3.S52', 3, 12): 	2,
-		('3.S52', 3, 13):	2,
-
-		('3.S6', 2, 11): 	2,
-		('3.S6', 2, 12): 	2,
-		('3.S6', 2, 13):	2,
-		('3.S6', 3, 11): 	2,
-		('3.S6', 3, 12): 	2,
-		('3.S6', 3, 13):	2,
+		('6.S10',2,11):   2,
+		('4.S6b',2,11):   2,
+		('7.S5a',2,11):   2,
+		('7.S5b',2,11):   2,
+		('4.S6a',2,11):   2,
+		('8.S6',2,11):   2,
+		('3.S6',2,11):   2,
+		('3.RE2a',2,11):   2,
+		('3.RE2b',2,11):   2,
+		('3.S5',2,11):   2,
+		('7.S14a',2,11):   2,
+		('7.S14b',2,11):   2,
+		('4.RE1',2,11):   2,
+		('2.RE3',2,11):   2,
+		('4.R5',2,11):   2,
+		('4.R6',2,11):   2,
+		('4.R7',2,11):   2,
+		('2.S40',2,11):   2,
+		('7.S3b',2,11):   2,
+		('7.S3a',2,11):   2,
+		('3.R1',2,11):   2,
+		('3.RE4',2,11):   2,
+		('3.R2',2,11):   2,
+		('3.S2a',2,11):   2,
+		('3.R7',2,11):   2,
+		('3.R6',2,11):   2,
+		('7.S25',2,11):   2,
+		('3.S44c',2,11):   2,
+		('7.S2a',2,11):   2,
+		('7.S2b',2,11):   2,
+		('1.R3',2,11):   2,
+		('1.R4',2,11):   2,
+		('3.S4b',2,11):   2,
+		('5.RE1',2,11):   2,
+		('5.S28',2,11):   2,
+		('7.S16b',2,11):   2,
+		('7.S16a',2,11):   2,
+		('5.S9',2,11):   2,
+		('3.S51',2,11):   2,
+		('3.S52',2,11):   2,
+		('6.S30',2,11):   2,
+		('2.RE4a',2,11):   2,
+		('2.RE4b',2,11):   2,
+		('6.S3',2,11):   2,
+		('6.S2',2,11):   2,
+		('6.S9',2,11):   2,
+		('2.S1',2,11):   2,
+		('7.S8c',2,11):   2,
+		('7.S8b',2,11):   2,
+		('7.S8a',2,11):   2,
+		('6.S20',2,11):   2,
+		('2.S30',2,11):   2,
+		('6.S25',2,11):   2,
+		('6.S26',2,11):   2,
+		('6.S27',2,11):   2,
+		('6.S29',2,11):   2,
+		('1.S2b',2,11):   2,
+		('1.S2a',2,11):   2,
+		('6.S23a',2,11):   2,
+		('6.S23b',2,11):   2,
+		('6.S41',2,11):   2,
+		('2.R8',2,11):   2,
+		('3.S3b',2,11):   2,
+		('3.S3a',2,11):   2,
+		('3.S4a',2,11):   2,
+		('5.S1',2,11):   2,
+		('2.R11',2,11):   2,
+		('3.RE1a',2,11):   2,
+		('5.S8',2,11):   2,
+		('5.S3a',2,11):   2,
+		('5.S3b',2,11):   2,
+		('5.S3c',2,11):   2,
+		('6.RE2',2,11):   2,
+		('7.S7b',2,11):   2,
+		('7.S7a',2,11):   2,
+		('3.S2b',2,11):   2,
+		('2.S9',2,11):   2,
+		('3.R4b',2,11):   2,
+		('3.R4a',2,11):   2,
+		('7.S15b',2,11):   2,
+		('7.S15a',2,11):   2,
+		('2.S4b',2,11):   2,
+		('2.S4a',2,11):   2,
+		('7.S24b',2,11):   2,
+		('7.S24c',2,11):   2,
+		('7.S24a',2,11):   2,
+		('2.R12',2,11):   2,
+		('7.S6a',2,11):   2,
+		('3.S44a',2,11):   2,
+		('3.S44b',2,11):   2,
+		('7.S6b',2,11):   2,
+		('3.S1a',2,11):   2,
+		('3.S1b',2,11):   2,
+		('3.RE1b',2,11):   2,
+		('2.R10',2,11):   2,
+		('7.S9a',2,11):   2,
+		('7.S9b',2,11):   2,
+		('2.R13',2,11):   2,
+		('2.R14',2,11):   2,
+		('7.S12b',2,11):   2,
+		('7.S12a',2,11):   2,
+		('1.S3a',2,11):   2,
+		('1.S3b',2,11):   2,
+		('8.RE1',2,11):   2,
+		('6.S10',3,11):   2,
+		('4.S6b',3,11):   2,
+		('7.S5a',3,11):   2,
+		('7.S5b',3,11):   2,
+		('4.S6a',3,11):   2,
+		('8.S6',3,11):   2,
+		('3.S6',3,11):   2,
+		('3.RE2a',3,11):   2,
+		('3.RE2b',3,11):   2,
+		('3.S5',3,11):   2,
+		('7.S14a',3,11):   2,
+		('7.S14b',3,11):   2,
+		('4.RE1',3,11):   2,
+		('2.RE3',3,11):   2,
+		('4.R5',3,11):   2,
+		('4.R6',3,11):   2,
+		('4.R7',3,11):   2,
+		('2.S40',3,11):   2,
+		('7.S3b',3,11):   2,
+		('7.S3a',3,11):   2,
+		('3.R1',3,11):   2,
+		('3.RE4',3,11):   2,
+		('3.R2',3,11):   2,
+		('3.S2a',3,11):   2,
+		('3.R7',3,11):   2,
+		('3.R6',3,11):   2,
+		('7.S25',3,11):   2,
+		('3.S44c',3,11):   2,
+		('7.S2a',3,11):   2,
+		('7.S2b',3,11):   2,
+		('1.R3',3,11):   2,
+		('1.R4',3,11):   2,
+		('3.S4b',3,11):   2,
+		('5.RE1',3,11):   2,
+		('5.S28',3,11):   2,
+		('7.S16b',3,11):   2,
+		('7.S16a',3,11):   2,
+		('5.S9',3,11):   2,
+		('3.S51',3,11):   2,
+		('3.S52',3,11):   2,
+		('6.S30',3,11):   2,
+		('2.RE4a',3,11):   2,
+		('2.RE4b',3,11):   2,
+		('6.S3',3,11):   2,
+		('6.S2',3,11):   2,
+		('6.S9',3,11):   2,
+		('2.S1',3,11):   2,
+		('7.S8c',3,11):   2,
+		('7.S8b',3,11):   2,
+		('7.S8a',3,11):   2,
+		('6.S20',3,11):   2,
+		('2.S30',3,11):   2,
+		('6.S25',3,11):   2,
+		('6.S26',3,11):   2,
+		('6.S27',3,11):   2,
+		('6.S29',3,11):   2,
+		('1.S2b',3,11):   2,
+		('1.S2a',3,11):   2,
+		('6.S23a',3,11):   2,
+		('6.S23b',3,11):   2,
+		('6.S41',3,11):   2,
+		('2.R8',3,11):   2,
+		('3.S3b',3,11):   2,
+		('3.S3a',3,11):   2,
+		('3.S4a',3,11):   2,
+		('5.S1',3,11):   2,
+		('2.R11',3,11):   2,
+		('3.RE1a',3,11):   2,
+		('5.S8',3,11):   2,
+		('5.S3a',3,11):   2,
+		('5.S3b',3,11):   2,
+		('5.S3c',3,11):   2,
+		('6.RE2',3,11):   2,
+		('7.S7b',3,11):   2,
+		('7.S7a',3,11):   2,
+		('3.S2b',3,11):   2,
+		('2.S9',3,11):   2,
+		('3.R4b',3,11):   2,
+		('3.R4a',3,11):   2,
+		('7.S15b',3,11):   2,
+		('7.S15a',3,11):   2,
+		('2.S4b',3,11):   2,
+		('2.S4a',3,11):   2,
+		('7.S24b',3,11):   2,
+		('7.S24c',3,11):   2,
+		('7.S24a',3,11):   2,
+		('2.R12',3,11):   2,
+		('7.S6a',3,11):   2,
+		('3.S44a',3,11):   2,
+		('3.S44b',3,11):   2,
+		('7.S6b',3,11):   2,
+		('3.S1a',3,11):   2,
+		('3.S1b',3,11):   2,
+		('3.RE1b',3,11):   2,
+		('2.R10',3,11):   2,
+		('7.S9a',3,11):   2,
+		('7.S9b',3,11):   2,
+		('2.R13',3,11):   2,
+		('2.R14',3,11):   2,
+		('7.S12b',3,11):   2,
+		('7.S12a',3,11):   2,
+		('1.S3a',3,11):   2,
+		('1.S3b',3,11):   2,
+		('8.RE1',3,11):   2,
 	}
 
 	# select a route in RouteInfoList1
@@ -293,99 +480,99 @@ if __name__ == '__main__':
 			totalCov += LMCoverageTotal.get(var,"")
 
 
-	print  float(totalCov) / float(totalReq) * 100
+	print "Coverage Percentage : %s" % (float(totalCov) / float(totalReq) * 100)
 
-	# **************************************************************************************
-	# Travel Segments
-	# **************************************************************************************
+	# # **************************************************************************************
+	# # Travel Segments
+	# # **************************************************************************************
 
-	print LineSeparator 
-	print "Travel Segments of a Route"
-	print LineSeparator
+	# print LineSeparator 
+	# print "Travel Segments of a Route"
+	# print LineSeparator
 
-	# Travel segments are defined by real change points:
-	# A travel segment is a continuous trip without a line change.
+	# # Travel segments are defined by real change points:
+	# # A travel segment is a continuous trip without a line change.
 
-	# get travel segments (Reisen) of Route1
-	TravelSegments1 = GetTravelSegments(Route1, TimeWindows)
+	# # get travel segments (Reisen) of Route1
+	# TravelSegments1 = GetTravelSegments(Route1, TimeWindows)
 
-	print "\nTravelSegments1 (raw print dictionary)"
-	PrintDictionaryContent(TravelSegments1)
+	# print "\nTravelSegments1 (raw print dictionary)"
+	# PrintDictionaryContent(TravelSegments1)
 
-	# TravelSegments1 is a dictionary whose values are lists with multiple field values.
-	# The meaning and order of these fields are defined in global variable SegmentInfoInd
-	# in BU2019_CentralParameters.py
+	# # TravelSegments1 is a dictionary whose values are lists with multiple field values.
+	# # The meaning and order of these fields are defined in global variable SegmentInfoInd
+	# # in BU2019_CentralParameters.py
 
-	# For example, this is how you can get the LineID of segment 1:
-	print "\nGet LineID of segment 1:"
-	LineID_seg1 = TravelSegments1[1][SegmentInfoInd['line_id']]
-	print "LineID_seg1 = %s" % LineID_seg1
+	# # For example, this is how you can get the LineID of segment 1:
+	# print "\nGet LineID of segment 1:"
+	# LineID_seg1 = TravelSegments1[1][SegmentInfoInd['line_id']]
+	# print "LineID_seg1 = %s" % LineID_seg1
 
-	# Keys of the dictionary TravelSegments1 are simply ordered segment numbers (1, 2, ... N)
+	# # Keys of the dictionary TravelSegments1 are simply ordered segment numbers (1, 2, ... N)
 
-	# With the following function, travel segments can be displayed nicely 
-	# together with the route information:
+	# # With the following function, travel segments can be displayed nicely 
+	# # together with the route information:
 
-	print "\nDisplay TravelSegments1 nicely together with route information"
-	print PrettyStringRouteSegmentsInfo(TravelSegments1)
+	# print "\nDisplay TravelSegments1 nicely together with route information"
+	# print PrettyStringRouteSegmentsInfo(TravelSegments1)
 
-	# **************************************************************************************
-	# Route Segments
-	# **************************************************************************************
+	# # **************************************************************************************
+	# # Route Segments
+	# # **************************************************************************************
 
-	print LineSeparator 
-	print "Route Segments of a Route"
-	print LineSeparator
+	# print LineSeparator 
+	# print "Route Segments of a Route"
+	# print LineSeparator
 
-	# Unlike travel segments, route segments can have virtual change points 
-	# for LineID and time window changes within the same continuous trip. 
-	# Route segments are the basis for measurement variants of a route.
-	RouteSegment1 =  GetRouteSegments(Route1, TimeWindows)
+	# # Unlike travel segments, route segments can have virtual change points 
+	# # for LineID and time window changes within the same continuous trip. 
+	# # Route segments are the basis for measurement variants of a route.
+	# RouteSegment1 =  GetRouteSegments(Route1, TimeWindows)
 
-	print "\nRouteSegment1 (raw)"
-	PrintDictionaryContent(RouteSegment1)
+	# print "\nRouteSegment1 (raw)"
+	# PrintDictionaryContent(RouteSegment1)
 
-	# Route segments too can be displayed nicely together with the route information:
+	# # Route segments too can be displayed nicely together with the route information:
 
-	print "\nDisplay RouteSegment1 nicely together with route information"
-	print PrettyStringExtendedRouteSegmentsInfo(RouteSegment1)
+	# print "\nDisplay RouteSegment1 nicely together with route information"
+	# print PrettyStringExtendedRouteSegmentsInfo(RouteSegment1)
 
-	print Route1
+	# print Route1
 
-	# Unless there are TimeWindow of LineID changes within continuous travels (Reisen, travel segments)
-	# of a route, route segments are expected to be same as travel segments. 
+	# # Unless there are TimeWindow of LineID changes within continuous travels (Reisen, travel segments)
+	# # of a route, route segments are expected to be same as travel segments. 
 
-	# Subsequent connections with the same FahrtID (trip_id) constitute a continuous 
-	# travel segment (Reise)
+	# # Subsequent connections with the same FahrtID (trip_id) constitute a continuous 
+	# # travel segment (Reise)
 
-	# Let's manipulate Route1 manually to display the difference of travel and route segments
-	# by adding LineID and TimeWindow changes to the route:
+	# # Let's manipulate Route1 manually to display the difference of travel and route segments
+	# # by adding LineID and TimeWindow changes to the route:
 
-	Route1_manipulated = [
-	(8500000, 8507000, None, None, None, None, None, 'W', None, 8, 0, None, 8, 0, None, None, None), 
-	(8507000, 8504489, 792580, '3.S5', 60995, 15528, 33, 'S', '5', 8, 8, 488, 8, 12, 492, '', 1), 
-	(8504489, 8516154, 792581, '3.S5', 60995, 15528, 33, 'S', '5', 8, 12, 492, 8, 14, 494, '', 2), 
+	# Route1_manipulated = [
+	# (8500000, 8507000, None, None, None, None, None, 'W', None, 8, 0, None, 8, 0, None, None, None), 
+	# (8507000, 8504489, 792580, '3.S5', 60995, 15528, 33, 'S', '5', 8, 8, 488, 8, 12, 492, '', 1), 
+	# (8504489, 8516154, 792581, '3.S5', 60995, 15528, 33, 'S', '5', 8, 12, 492, 8, 14, 494, '', 2), 
 
-	# extend the duration of the trip to cause a TimeWindow change
-	# within the same travel segment (notice same FahrtID 61373 of following connections)
-	(8516154, 8504489, 795307, '3.S52', 61373, 16227, 33, 'S', '52', 8, 18, 498, 12, 0, 720, '', 5), 
-	(8504489, 8507000, 795308, '3.S52', 61373, 16227, 33, 'S', '52', 12, 5, 725, 14, 26, 906, '', 6), 
-	(8507000, 8503000, 48462, None, 4581, 709, 11, 'IC', '1', 14, 30, 910, 14, 45, 935, '', 2)]
+	# # extend the duration of the trip to cause a TimeWindow change
+	# # within the same travel segment (notice same FahrtID 61373 of following connections)
+	# (8516154, 8504489, 795307, '3.S52', 61373, 16227, 33, 'S', '52', 8, 18, 498, 12, 0, 720, '', 5), 
+	# (8504489, 8507000, 795308, '3.S52', 61373, 16227, 33, 'S', '52', 12, 5, 725, 14, 26, 906, '', 6), 
+	# (8507000, 8503000, 48462, None, 4581, 709, 11, 'IC', '1', 14, 30, 910, 14, 45, 935, '', 2)]
 
-	# Now compare the travel and tour segments of the manipulated tour Route1_manipulated
-	print "\nDisplay manipulated route Route1_manipulated"
-	print PrettyStringRouteInfo(Route1_manipulated)
+	# # Now compare the travel and tour segments of the manipulated tour Route1_manipulated
+	# print "\nDisplay manipulated route Route1_manipulated"
+	# print PrettyStringRouteInfo(Route1_manipulated)
 
-	TravelSegments1_man = GetTravelSegments(Route1_manipulated, TimeWindows)
-	print "\nDisplay TravelSegments of Route1_manipulated nicely together with route information"
-	print PrettyStringRouteSegmentsInfo(TravelSegments1_man)
+	# TravelSegments1_man = GetTravelSegments(Route1_manipulated, TimeWindows)
+	# print "\nDisplay TravelSegments of Route1_manipulated nicely together with route information"
+	# print PrettyStringRouteSegmentsInfo(TravelSegments1_man)
 
-	RouteSegment1_man =  GetRouteSegments(Route1_manipulated, TimeWindows)
-	print "\nDisplay RouteSegments of Route1_manipulated nicely together with route information"
-	print PrettyStringExtendedRouteSegmentsInfo(RouteSegment1_man)
+	# RouteSegment1_man =  GetRouteSegments(Route1_manipulated, TimeWindows)
+	# print "\nDisplay RouteSegments of Route1_manipulated nicely together with route information"
+	# print PrettyStringExtendedRouteSegmentsInfo(RouteSegment1_man)
 
-	# Note that in the Route1_manipulated, we have additional route segments due to 
-	# LineID and TimeWindow changes within the same travel segment.
+	# # Note that in the Route1_manipulated, we have additional route segments due to 
+	# # LineID and TimeWindow changes within the same travel segment.
 
 	# **************************************************************************************
 	# Valuation and Sorting of Routes
@@ -453,25 +640,25 @@ if __name__ == '__main__':
 	# Availability of Routes
 	# **************************************************************************************
 
-	print LineSeparator 
-	print "Availability of Routes (on week days)"
-	print LineSeparator
+	# print LineSeparator 
+	# print "Availability of Routes (on week days)"
+	# print LineSeparator
 
-	# get availability of Route1
+	# # get availability of Route1
 
-	# StartDate: First day of month as date object 
-	# EndDate: last day of month as date object
-	# StartDate & EndDate are global variables defined in PlanInputParameters.py
-	(AvailableDaysRoute, UnavailableDaysRoute) = GetAvailabilityOfRoute(Route1, StartDate, EndDate)
+	# # StartDate: First day of month as date object 
+	# # EndDate: last day of month as date object
+	# # StartDate & EndDate are global variables defined in PlanInputParameters.py
+	# (AvailableDaysRoute, UnavailableDaysRoute) = GetAvailabilityOfRoute(Route1, StartDate, EndDate)
 
-	# AvailableDaysRoute and UnavailableDaysRoute are lists of ordinal dates (day numbers)
+	# # AvailableDaysRoute and UnavailableDaysRoute are lists of ordinal dates (day numbers)
 
-	print "\nRoute1 is available on days (%s):" % len(AvailableDaysRoute)
-	for DayOrd in AvailableDaysRoute:
-		print "Date: %s - Weekday: %s" % (ConvertDateOrdinalToDateString(DayOrd), GetWeekdayOfDate(DayOrd))
+	# print "\nRoute1 is available on days (%s):" % len(AvailableDaysRoute)
+	# for DayOrd in AvailableDaysRoute:
+	# 	print "Date: %s - Weekday: %s" % (ConvertDateOrdinalToDateString(DayOrd), GetWeekdayOfDate(DayOrd))
 
-	print "\nRoute1 is NOT available on days (%s):" % len(UnavailableDaysRoute)
-	for DayOrd in UnavailableDaysRoute:
-		print "Date: %s - Weekday: %s" % (ConvertDateOrdinalToDateString(DayOrd), GetWeekdayOfDate(DayOrd))
+	# print "\nRoute1 is NOT available on days (%s):" % len(UnavailableDaysRoute)
+	# for DayOrd in UnavailableDaysRoute:
+	# 	print "Date: %s - Weekday: %s" % (ConvertDateOrdinalToDateString(DayOrd), GetWeekdayOfDate(DayOrd))
 
 	
